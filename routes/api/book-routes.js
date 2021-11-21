77adb0d76a0fd25e23c094321c7e67a926d0d86c
Varsha Jain <csvarshajain@gmail.com>
2021-11-21 13:49:50 -0500

    added get and post requests for book

--- routes/api/book-routes.js
+++ routes/api/book-routes.js
@@ -1,20 +1,50 @@
 const router = require('express').Router();
-const { Book } = require('../../models')
+const { Book } = require('../../models/index')
 
 //get api/books routes
 
 router.get('/', (req, res) => {
-
+    Book.findAll().then(dbBookData => res.json(dbBookData))
+        .catch(err => {
+            console.log(err);
+            re.status(500).json(err)
+        })
 
 })
 //get api/books/1 routes
 router.get('/:id', (req, res) => {
-
+    UPSERT.findOne({
+        where: {
+            id: req.params.id
+        }
+    }).then(dbBookData => {
+        if (!dbBookData) {
+            res.status(404)({ message: 'No data Found' });
+            return
+        }
+        res.json(dbBookData)
+    })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 })
+
 
 //post api/books routes
 router.post('/', (req, res) => {
+    Book.create({
+        title: req.body.title,
+        author: req.body.author,
+        publish: req.body.publish
 
+    }).then(dbBookData => {
+        console.log(dbBookData)
+        res.json(dbBookData)
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
 })
 
 //update api/books/1 routes
